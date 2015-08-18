349a21d2d512c04193f54e4e83c59c5ff349f1f8
Mehdi Abaakouk <sileht@sileht.net>
2015-08-18 17:30:10 +0200

    Update release script

--- releases.sh
+++ releases.sh
@@ -7,8 +7,17 @@ git tag -s $version -m "Release version ${version}"
 git checkout $version
 git clean -fd
 rm -rf python_jsonpath_rw_ext.egg-info build dist
-tox -evenv python setup.py sdist bdist_wheel
+tox -r -evenv python setup.py sdist bdist_wheel
 
+echo "release: python-jsonpath-rw-ext ${version}"
+echo
+echo "SHA1sum: "
+sha1sum dist/*
+echo "MD5sum: "
+md5sum dist/*
 
-echo "git push --tags"
-echo "twine upload -r pypi dist/python-jsonpath-rw-ext-${version}.tar.gz"
+echo 
+echo "To publish:"
+echo
+echo "* git push --tags"
+echo "* twine upload -r pypi -s dist/python-jsonpath-rw-ext-${version}.tar.gz dist/python_jsonpath_rw_ext-${version}-py2.py3-none-any.whl"
