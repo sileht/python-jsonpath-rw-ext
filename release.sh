dec2609d9a6a6de4077e8e452579d41260d73f44
Mehdi Abaakouk <sileht@sileht.net>
2017-02-25 22:51:11 +0100

    Update release script and fix doc

---
+++ release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -x
+
+version=$1
+[ ! "$version"] && version=$(python setup.py --version | sed 's/\.dev.*//')
+
+status=$(git status -sz)
+[ -z "$status" ] || false
+git checkout master
+tox -epy35,py27,pep8,docs
+git push 
+git tag -s $version -m "Release version ${version}"
+git checkout $version
+git clean -fd
+pbr_version=$(python setup.py --version)
+if [ "$version" != "$pbr_version" ]; then
+    echo "something goes wrong pbr version is different from the provided one. ($pbr_version != $version)"
+    exit 1
+fi
+python setup.py sdist bdist_wheel
+
+set +x
+echo
+echo "release: jsonpath-rw-ext ${version}"
+echo
+echo "SHA1sum: "
+sha1sum dist/*
+echo "MD5sum: "
+md5sum dist/*
+echo
+echo "uploading..."
+echo
+set -x
+
+read
+git push --tags
+twine upload -r pypi -s dist/jsonpath-rw-ext-${version}.tar.gz dist/jsonpath_rw_ext-${version}-py2.py3-none-any.whl
+git checkout master
